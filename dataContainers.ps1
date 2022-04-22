6fae1b95efee746a20cb8083a0bdacd0ec1ffa69
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-04-22 13:35:23 +0200

    introduce array and hashtable

---
+++ dataContainers.ps1
@@ -0,0 +1,21 @@
+﻿#@(Liste von Elementen) ist das Array-Literal
+$names = @("Hugo", "Emil", "Fritz")
+$name = $names[0] #-> Hugo
+$name = $names[2] #-> Fritz
+$name = $names[42] #-> 'null'
+$name = $names[-2] #-> Emil
+#Write-Output $name
+
+$nameRange = $names[1..2]
+#Write-Output $nameRange
+
+
+#@{Liste von key=value-Paare} ist das Hashtable-Literal
+$nameToCity = @{"Hugo"="Berlin";"Emil"="München";"Fritz"="Stuttgart"}
+$city = $nameToCity["Emil"] #-> München
+$city = $nameToCity["Eduard"]#->null
+#Write-Output $city
+$cities= $nameToCity["Emil", "Hugo"]
+Write-Output $cities[0]
+
+
--- solution_consol_input_and_if.ps1
+++ solution_consol_input_and_if.ps1
@@ -1,9 +1,9 @@
 ﻿$input = Read-Host "Please enter a number"
 $number = [int]$input
 if ($number -lt 10){
-    Write-Host "you entered $input, $number is less than 10"
+    Write-Host "you entered $number, $number is less than 10"
 }
 else{
-    Write-Host "you entered $input, $number is greater than or equals 10"
+    Write-Host "you entered $number, $number is greater than or equals 10"
 
 }
