1de12678f0a51cb24427de8718817368af44aa9a
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-04-22 11:14:30 +0200

    add variables

---
+++ variables.ps1
@@ -0,0 +1,10 @@
+﻿# Demo für Variablen
+
+# Variablen beginnen mit $ und einem anschließenden frei wählbaren Namen
+# Zuweisung mit =
+# Rechte Seite der Zuweisung: Literal
+$lastname = "Sawitzki"
+$firstname = "Rainer"
+
+$name = "$firstname $lastname"
+Write-Output $name
