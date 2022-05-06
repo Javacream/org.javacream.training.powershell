8b86d6be36262c31116bfc49463791821469958c
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-05-06 14:14:04 +0200

    improce db

--- db-scratch.ps1
+++ db-scratch.ps1
@@ -1,5 +1,21 @@
-﻿try{
-    Open-MySqlConnection -Password user -UserName user -Database javacream -Port 3406 -Server h2908727.stratoserver.net -SSLMode Required
+﻿function TrainingGet-Credential {
+    $password = ConvertTo-SecureString "user" -AsPlainText -Force 
+    return New-Object System.Management.Automation.PSCredential("user", $password)
+}
+
+try{
+    $credential = TrainingGet-Credential
+    Open-MySqlConnection -Credential $credential -Database javacream -Port 3406 -Server h2908727.stratoserver.net -SSLMode Required
+    Start-SqlTransaction
+        Invoke-SqlUpdate -Query "insert into messages values ('Hugo2')"
+    #Complete-SqlTransaction
+    Undo-SqlTransaction
+
+    Start-SqlTransaction
+        $data = Invoke-SqlQuery -Query "select * from messages"
+        Write-Output $data
+    Complete-SqlTransaction
+
 }
 catch{
     Write-Output $_
