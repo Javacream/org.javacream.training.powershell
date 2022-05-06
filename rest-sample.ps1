ff499a800bde3e9a4fec933ba7fbddd5d8960dc2
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-05-06 09:55:21 +0200

    improve rest sample

--- rest-sample.ps1
+++ rest-sample.ps1
@@ -1,4 +1,11 @@
-﻿$books = Invoke-RestMethod -Uri http://h2908727.stratoserver.net:8080/api/book
-foreach($book in $books){
-    Write-Output $book.price
+﻿Remove-Variable * -ErrorAction SilentlyContinue
+try{
+    $books = Invoke-RestMethod -Uri http://h2908727.stratoserver.net:8080/api/books
+    foreach($book in $books){
+        Write-Output $book.title
+    }
 }
+catch{
+    Write-Output "error: $_"
+}
+
