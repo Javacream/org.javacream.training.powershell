8d802f9b3d4ef99cb8f1de13214be6d8c7058a9b
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-05-06 11:04:29 +0200

    introduce with functions

---
+++ functions_demo.ps1
@@ -0,0 +1,20 @@
+﻿Remove-Variable * -ErrorAction SilentlyContinue
+function TrainingDo-DemoArgs{
+    #Write-Output "$p1 $p2"
+    Write-Output $args.Length
+
+}
+function TrainingDo-DemoParams($p1, $p2){
+    Write-Output "$p1 $p2"
+
+}
+
+function TrainingDo-DemoParams($p1, $p2){
+    $functionResult = "$p1 $p2"
+    return $functionResult
+}
+
+
+$result = TrainingDo-DemoParams Hello World
+Write-Output $result
+Write-Output $p1 $functionResult
