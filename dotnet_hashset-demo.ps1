57c01ae4159d9b8419359bd16b99be5dd3bf2811
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-05-06 10:35:13 +0200

    add .NET sample

---
+++ dotnet_hashset-demo.ps1
@@ -0,0 +1,9 @@
+﻿$dotnetObject = New-Object System.Collections.Generic.HashSet[string]
+$dotnetObject.Add("Hugo")
+$dotnetObject.Add("Fritz")
+$dotnetObject.Add("Hugo")
+$dotnetObject.Add("Emil")
+foreach($name in $dotnetObject){
+    Write-Output $name
+}
+
