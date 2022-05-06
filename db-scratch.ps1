16e041dbbf217aeaee54c531ea8294e7c3d260b6
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-05-06 13:48:12 +0200

    start db

---
+++ db-scratch.ps1
@@ -0,0 +1,9 @@
+﻿try{
+    Open-MySqlConnection -Password user -UserName user -Database javacream -Port 3406 -Server h2908727.stratoserver.net -SSLMode Required
+}
+catch{
+    Write-Output $_
+}
+finally{
+    Close-SqlConnection
+}
